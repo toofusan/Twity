480eb2af6619d59ca91a4f6b8d5bddec299eb0d1
toofusan <toru.yoshioka+github@c-fo.com>
2018-07-22 09:12:56 +0900

    Add Url to Entities DataModel

---
+++ DataModels/Entities/Url.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Twity.DataModels.Entities
+{
+    [Serializable]
+    public class Url
+    {
+        public string url;
+        public string display_url;
+        public string expanded_url;
+    }
+}
