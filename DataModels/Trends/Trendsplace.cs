fed6ee1b0f9d8f26eb568b9c642b70e02b4958bb
Toru Yoshioka <toofu@torunombookpuro.lan>
2018-09-29 21:06:40 +0900

    Add Trend class

---
+++ DataModels/Trends/Trendsplace.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using Twity.DataModels.Trend;
+
+namespace Twity.DataModels.Trend {
+
+	[Serializable]
+	public class TrendsPlace
+	{
+		public Trend[] trends;
+		public string as_of;
+		public string created_at;
+		public Woeid_location locaions;
+	}
+}
---
+++ DataModels/Trends/Woeid_location.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Twity.DataModels.Trend
+{
+    [Serializable]
+    public class Woeid_location
+    {
+        public string name;
+		public int woeid;
+    }
+}
