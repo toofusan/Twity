c74ed6ad81a0eaf5be10796b63736c85d766da04
Toru Yoshioka <toofu@torunombookpuro.lan>
2018-09-29 21:19:19 +0900

    add response class

---
+++ DataModels/Responses/TrendsPlaceResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Twity.DataModels.Trends;
+
+namespace Twity.DataModels.Responses
+{
+    [Serializable]
+    public class TrendsPlaceResponse
+    {
+        public TrendsPlace[] items;
+    }
+}
