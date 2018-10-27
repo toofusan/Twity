71ff4d094b08e311bdab338fbb758e69a691e577
toofusan <toru.yoshioka+github@c-fo.com>
2018-10-27 19:33:59 +0900

    fix helper methods

--- Scripts/TwitterOauth.cs
+++ Scripts/TwitterOauth.cs
@@ -77,43 +77,6 @@ namespace Twity
             return Convert.ToBase64String(signatureBytes);
         }
 
-        // The below helper methods are modified from "WebRequestBuilder.cs" in Twitterizer(http://www.twitterizer.net/).
-        // Here is its license.
-
-        //-----------------------------------------------------------------------
-        // <copyright file="WebRequestBuilder.cs" company="Patrick 'Ricky' Smith">
-        //  This file is part of the Twitterizer library (http://www.twitterizer.net/)
-        //
-        //  Copyright (c) 2010, Patrick "Ricky" Smith (ricky@digitally-born.com)
-        //  All rights reserved.
-        //
-        //  Redistribution and use in source and binary forms, with or without modification, are
-        //  permitted provided that the following conditions are met:
-        //
-        //  - Redistributions of source code must retain the above copyright notice, this list
-        //    of conditions and the following disclaimer.
-        //  - Redistributions in binary form must reproduce the above copyright notice, this list
-        //    of conditions and the following disclaimer in the documentation and/or other
-        //    materials provided with the distribution.
-        //  - Neither the name of the Twitterizer nor the names of its contributors may be
-        //    used to endorse or promote products derived from this software without specific
-        //    prior written permission.
-        //
-        //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-        //  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-        //  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-        //  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-        //  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-        //  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-        //  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-        //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-        //  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-        //  POSSIBILITY OF SUCH DAMAGE.
-        // </copyright>
-        // <author>Ricky Smith</author>
-        // <summary>Provides the means of preparing and executing Anonymous and OAuth signed web requests.</summary>
-        //-----------------------------------------------------------------------
-
         private static void AddDefaultOauthParams(SortedDictionary<string, string> parameters, string consumerKey)
         {
             parameters.Add("oauth_consumer_key", consumerKey);
@@ -125,13 +88,13 @@ namespace Twity
 
         private static string GenerateTimeStamp()
         {
-            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            return Convert.ToInt64(ts.TotalSeconds).ToString();
+            DateTimeOffset baseDt = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+            return ((DateTimeOffset.Now - baseDt).Ticks/10000000).ToString();
         }
 
         private static string GenerateNonce()
         {
-            return new System.Random().Next(123400, int.MaxValue).ToString("X");
+            return Guid.NewGuid().ToString("N");
         }
 
         #endregion
